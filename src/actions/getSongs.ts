70bcf5e0ec76a677db4e69988cfe6fec42f0324f
kingsuk <kingsuk@github.com>
2024-03-05 11:46:56 +0530

    feat  - create home page

---
+++ src/actions/getSongs.ts
@@ -0,0 +1,22 @@
+import { Song } from "@/types/types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+const getSongs = async (): Promise<Song[]> => {
+  const supabse = createServerComponentClient({
+    cookies: cookies,
+  });
+
+  const { data, error } = await supabse
+    .from("songs")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    console.log(error.message);
+  }
+
+  return (data as any) || [];
+};
+
+export default getSongs;
---
+++ src/hooks/useUser.tsx
@@ -0,0 +1,10 @@
+import { UserContext } from "@/providers/MyContextProvider";
+import { useContext } from "react";
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error(`useUser must be used within a MyUserContextProvider.`);
+  }
+  return context;
+};
