d2c12e139c0f01103f15df886876911c187c8d8c
kingsuk <kingsuk@github.com>
2024-03-05 21:14:50 +0530

    feat - create search page

---
+++ src/components/search/SearchInput.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/navigation";
+import queryString from "query-string";
+import { useEffect, useState } from "react";
+import Input from "../ui/Input";
+
+const SearchInput = () => {
+  const router = useRouter();
+  const [value, setValue] = useState<string>("");
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  useEffect(() => {
+    const query = {
+      title: debouncedValue,
+    };
+
+    const url = queryString.stringifyUrl({
+      url: "/search",
+      query,
+    });
+
+    router.push(url);
+  }, [debouncedValue, router]);
+  return (
+    <Input
+      placeholder="what do you want to listen to?"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+};
+
+export default SearchInput;
