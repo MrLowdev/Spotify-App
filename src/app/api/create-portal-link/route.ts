5437a4a09364389b6587b2ccb33daa61994128aa
kingsuk <kingsuk@github.com>
2024-03-06 10:12:36 +0530

    feat -create player

--- src/app/api/create-portal-link/route.ts
+++ src/app/api/create-portal-link/route.ts
@@ -1,9 +1,11 @@
-import { stripe } from "@/lib/stripe";
-import { createOrRetrieveCustomer } from "@/lib/supabaseAdmin";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { getURL } from "next/dist/shared/lib/utils";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
+
+import { stripe } from "@/lib/stripe";
+import { getURL } from "@/lib/helper";
+import { createOrRetrieveCustomer } from "@/lib/supabaseAdmin";
+
 export async function POST() {
   try {
     const supabase = createRouteHandlerClient({
@@ -14,27 +16,21 @@ export async function POST() {
       data: { user },
     } = await supabase.auth.getUser();
 
-    if (!user) {
-      throw Error("could not get user");
-    }
-
+    if (!user) throw Error("Could not get user");
     const customer = await createOrRetrieveCustomer({
       uuid: user.id || "",
       email: user.email || "",
     });
 
-    if (!customer) {
-      throw Error("could not get customer");
-    }
-
+    if (!customer) throw Error("Could not get customer");
     const { url } = await stripe.billingPortal.sessions.create({
       customer,
       return_url: `${getURL()}/account`,
     });
 
     return NextResponse.json({ url });
-  } catch (error) {
-    console.log(error);
+  } catch (err: any) {
+    console.log(err);
     new NextResponse("Internal Error", { status: 500 });
   }
 }
